c0eb0c482c2a553eb169fa4dbda98ef2f19aeb41
Jean <bedrac.zan@gmail.com>
2021-12-09 09:48:18 +0100

    removed functions 'handle_server' from vpis, izpis and zapri

--- imenik_izpis/src/main.rs
+++ imenik_izpis/src/main.rs
@@ -1,28 +1,21 @@
 use std::env;
 use std::io::prelude::*;
 use std::os::unix::net::UnixStream;
-use std::path::Path;
 
 // max bytes size
 const MAX_SIZE: usize = 1024;
 
 fn main() {
     // parse arguments
-    let args: Vec<String> = env::args().collect();
+    let mut args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         eprintln!("Error: Incorrect number of arguments.");
         return;
     }
 
     // connect to server
-    let path = Path::new("/tmp/s");
-    let stream = UnixStream::connect(path).unwrap();
+    let mut stream = UnixStream::connect("/tmp/s").unwrap();
 
-    // send data to server
-    handle_server(&stream, args);
-}
-
-fn handle_server(mut stream: &UnixStream, mut args: Vec<String>) {
     // write message to server
     args[0] = args[0][13..].to_string(); // remove target/build/
     let mut message = args.join(".");
