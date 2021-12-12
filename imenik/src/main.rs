7b14cf946d81f0c091ac2bb86be40d7317007467
Jean <bedrac.zan@gmail.com>
2021-12-12 15:55:48 +0100

    added shared memory

--- imenik/src/main.rs
+++ imenik/src/main.rs
@@ -4,6 +4,8 @@ use std::os::unix::net::{UnixListener, UnixStream};
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::ptr;
+use rand::Rng;
 
 mod user;
 use user::PhoneBook;
@@ -16,7 +18,7 @@ fn main() {
     let path = Path::new("/tmp/s");
     let pb = PhoneBook::new();
     let pb = Arc::new(Mutex::new(pb));
-    let mut open = true;
+    let open = Arc::new(Mutex::new(true));
 
     if path.exists() {
         match fs::remove_file(path) {
@@ -33,31 +35,39 @@ fn main() {
     println!("Listening for clients ...");
 
     for stream in socket.incoming() {
-        println!("is open: {}", open);
+        {
+            let stream = stream.unwrap();
+            let pb = Arc::clone(&pb);
+            let open = Arc::clone(&open);
 
-        let stream = stream.unwrap();
-        let pb = Arc::clone(&pb);
+            let handle = thread::spawn(move || {
+                // println!("\n* THREAD START *");
+                
+                let mut pb = pb.lock().unwrap();
+                let mut open = open.lock().unwrap();
+                handle_client(stream, &mut pb, &mut open);
+            });
 
-        if open == false {
-            break;
+            handle.join().unwrap();
+
+            // only thread join, mutex drop is automatic
+            /* let result = match handle.join() {
+                Ok(_) => "* THREAD CLOSED *".to_string(),
+                Err(e) => format!("Thread error occured: {:?}", e)
+            };
+
+            println!("{}", result); */
         }
 
-        let handle = thread::spawn(move || {
-            println!("\n* THREAD START *");
-            let mut pb = pb.lock().unwrap();
-            handle_client(stream, &mut pb, &mut open);
-        });
+        // close server if open is false
+        {
+            let open = Arc::clone(&open);
+            let open = open.lock().unwrap();
 
-        // only thread join, mutex drop is automatic
-        let result = match handle.join() {
-            Ok(_) => "* THREAD CLOSED *".to_string(),
-            Err(e) => match e.downcast::<String>() {
-                Ok(message) => format!("Panic happened: {}", message),
-                Err(_) => format!("Panic happened: unknown type."),
-            },
-        };
-
-        println!("{}", result);
+            if *open == false {
+                break;
+            }
+        }
     }
 
     println!("Server closed.");
@@ -92,7 +102,13 @@ fn handle_client(mut stream: UnixStream, pb: &mut PhoneBook, open: &mut bool) {
         "imenik_iskanje" => {
             let users = pb.search(&args[1]);
             let response = format!("Found {} users\n{}", users.len(), users.join("\n"));
-            response
+            let size = response.len();
+
+            // shared memory
+            let key = generate_shm_key().to_string();
+            create_shm(key.clone(), response);
+            
+            format!("{}.{}.", key, size)
         }
         _ => "Wrong header argument.".to_string(),
     };
@@ -100,4 +116,44 @@ fn handle_client(mut stream: UnixStream, pb: &mut PhoneBook, open: &mut bool) {
 
     // clear stream
     stream.flush().unwrap();
+}
+
+fn generate_shm_key() -> u16 {
+    let mut rang = rand::thread_rng();
+    let number: u16 = rang.gen();
+    return number;
+}
+
+fn create_shm(key: String, response: String) {
+    unsafe {
+        // data
+        let data = response.as_bytes();
+        let ptr = data.as_ptr() as *const libc::c_void;
+        let size: libc::size_t = response.len();
+
+        // name of shared memory becomes our key
+        let name: *const libc::c_char = key.as_ptr() as *const libc::c_char;
+        
+        // open shared memory
+        let fd = libc::shm_open(name, libc::O_RDWR | libc::O_CREAT, libc::S_IRUSR | libc::S_IWUSR);
+        if fd == -1 {
+            println!("Error: cannot open shared memory.");
+        }
+
+        // set size of shared memory
+        let response = libc::ftruncate(fd, size as libc::off_t);
+        if response == -1 {
+            println!("Error: cannot create shared memory.");
+        }
+
+        // map to shared memory
+        let address = libc::mmap(ptr::null_mut(), size, libc::PROT_WRITE, libc::MAP_SHARED, fd, 0);
+        if address == libc::MAP_FAILED {
+            println!("Error: cannot map to shared memory.");
+        }
+        
+        // copy data to shared memory
+        libc::memcpy(address, ptr, data.len());
+        libc::close(fd);
+    }
 }
--- imenik_iskanje/src/main.rs
+++ imenik_iskanje/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::io;
 use std::io::prelude::*;
 use std::os::unix::net::UnixStream;
+use std::{ptr, str};
 
 // max bytes size
 const MAX_SIZE: usize = 1024;
@@ -38,8 +39,50 @@ fn main() {
 
         // receive response from server
         let mut buffer = vec![0; MAX_SIZE];
-        stream.read(&mut buffer[..]).unwrap();
-        let message = String::from_utf8(buffer[..].to_vec()).unwrap();
-        println!("# Server: {}", message);
+        stream.read(&mut buffer).unwrap();
+        let message = String::from_utf8(buffer).unwrap();
+
+        // parse arguments
+        let args: Vec<String> = message
+        .split(".")
+        .map(|arg| arg.trim().to_string())
+        .collect();
+
+        // read from shared memory
+        let key = args[0].to_string();
+        let size = args[1].to_string().parse::<usize>().unwrap();
+        println!("size: {}, key: {}", size, key);
+
+        read_shm(key, size);
+    }
+}
+
+fn read_shm(key: String, size: usize) {
+    unsafe {
+        let name: *const libc::c_char = key.as_ptr() as *const libc::c_char;
+        let size: libc::size_t = size - 1;
+
+        let fd = libc::shm_open(name, libc::O_RDWR | libc::O_CREAT, libc::S_IRUSR | libc::S_IWUSR);
+        if fd == -1 {
+            println!("Error: cannot open shared memory.");
+        }
+
+        // map to shared memory
+        let address = libc::mmap(ptr::null_mut(), size, libc::PROT_READ, libc::MAP_SHARED, fd, 0);
+        if address == libc::MAP_FAILED {
+            println!("Error: cannot map to shared memory.");
+        }
+
+        // data buffer
+        let mut data  = vec![0_u8; size];
+        let ptr = data.as_mut_ptr() as *mut libc::c_char;
+        libc::strncpy(ptr, address as *const libc::c_char, size);
+
+        // unlink shared memory and close connection
+        libc::shm_unlink(name);
+        libc::close(fd);
+
+        // response
+        println!("# Server: {}", str::from_utf8(&data).unwrap());
     }
 }
