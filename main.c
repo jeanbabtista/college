798d2fc8cad8acc916fb21c16c7e996bdea2c9be
Jean <bedrac.zan@gmail.com>
2021-12-15 01:09:14 +0100

    second task completed

--- main.c
+++ main.c
@@ -3,7 +3,22 @@
 int main() {
     const unsigned size = 10;
     int* array = (int*)my_malloc(size * sizeof(int));
+    printf("\n&array: %lu\n", (long)array % (10u * 10 * 10 * 10));
+
     char* abc = (char*)my_malloc(size * sizeof(char));
+    printf("\n&char[]: %lu\n", (long)abc % (10u * 10 * 10 * 10));
+
+    const unsigned new_size = 1100;
+    int* new_array = (int*)my_malloc(new_size * sizeof(int));
+    printf("\n&new array: %lu\n", (long)new_array % (10u * 10 * 10 * 10));
+
+    int* _array = (int*)my_malloc(size * sizeof(int));
+    printf("\n&array: %lu\n", (long)_array % (10u * 10 * 10 * 10));
+
+    my_free(new_array);
+    my_free(array);
+    my_free(abc);
+    my_free(_array);
 
     /* // init
     for (unsigned i = 0; i < size; i++)
@@ -20,8 +35,5 @@ int main() {
     printf("Alphabet: ");
     for (unsigned i = 0; i < size; i++)
         printf("%c ", abc[i]);
-    printf("\n");
-
-    my_free(array);
-    my_free(abc); */
+    printf("\n"); */
 }
--- memory.c
+++ memory.c
@@ -15,49 +15,62 @@ struct segment_info {
 alloc_info* ptr_alloc_info;
 static int is_first_alloc = 1;
 
-void* my_malloc(const unsigned size) {
-    // config
-    const unsigned size_data = size;
-    const unsigned size_alloc_info = sizeof(alloc_info);
-    const unsigned size_segment_info = sizeof(segment_info);
-    const unsigned size_page = getpagesize();
-
-    printf("config:\n");
-    printf("\t-> data size: %d\n", size);
-    printf("\t-> sizeof(alloc_info): %d\n", size_alloc_info);
-    printf("\t-> sizeof(segment_info): %d\n", size_segment_info);
-    printf("\t-> page size: %d\n", size_page);
+void* my_malloc(const unsigned size_user_data) {
+    printf("\n---------- MY MALLOC ---------");
 
     if (is_first_alloc) {
         is_first_alloc = 0;
 
-        // round size_total to multiple of size_page
-        const unsigned size_actual = /* size_data + */ size_alloc_info;
-        const unsigned size_total = size_actual + size_page - size_actual % size_page;
+        // round size_total to multiple of getpagesize()
+        const unsigned size_total = sizeof(alloc_info) + getpagesize() - sizeof(alloc_info) % getpagesize();
 
         // allocate memory
         ptr_alloc_info = (alloc_info*)mmap(NULL, size_total, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-        ptr_alloc_info->size_actual = size_actual;
+        ptr_alloc_info->size_actual = sizeof(alloc_info);
         ptr_alloc_info->size_total = size_total;
         ptr_alloc_info->num_of_segments = 0;
         ptr_alloc_info->ptr_first_free_segment = ptr_alloc_info + 1;
 
         printf("\nalloc_info:\n");
-        printf("\t-> address: %lu\n", (long)ptr_alloc_info);
+        printf("\t-> address: %lu\n", (long)ptr_alloc_info % (10u * 10 * 10 * 10));
         printf("\t-> actual size: %d\n", ptr_alloc_info->size_actual);
         printf("\t-> total size: %d\n", ptr_alloc_info->size_total);
         printf("\t-> number of segments: %d\n", ptr_alloc_info->num_of_segments);
-        printf("\t-> first free segment: %lu\n", (long)ptr_alloc_info->ptr_first_free_segment);
+        printf("\t-> first free segment: %lu\n", (long)ptr_alloc_info->ptr_first_free_segment % (10u * 10 * 10 * 10));
+    }
+
+    // if no space is available at current page for new segment, map new memory
+    const int space_left = ptr_alloc_info->size_total - ptr_alloc_info->size_actual - size_user_data - sizeof(segment_info);
+    printf("\nSpace left: %d\n", space_left);
+    if (space_left <= 0) {
+        // round size_total to multiple of getpagesize()
+        const unsigned size_total = size_user_data + sizeof(alloc_info) + getpagesize() - (size_user_data + sizeof(alloc_info)) % getpagesize();
+
+        // allocate memory
+        ptr_alloc_info = (alloc_info*)mmap(NULL, size_total, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        ptr_alloc_info->size_actual = sizeof(alloc_info);
+        ptr_alloc_info->size_total = size_total;
+        ptr_alloc_info->num_of_segments = 0;
+        ptr_alloc_info->ptr_first_free_segment = ptr_alloc_info + 1;
+
+        printf("\nReserved more space:");
+        printf("\nalloc_info:\n");
+        printf("\t-> address: %lu\n", (long)ptr_alloc_info % (10u * 10 * 10 * 10));
+        printf("\t-> actual size: %d\n", ptr_alloc_info->size_actual);
+        printf("\t-> total size: %d\n", ptr_alloc_info->size_total);
+        printf("\t-> number of segments: %d\n", ptr_alloc_info->num_of_segments);
+        printf("\t-> first free segment: %lu\n", (long)ptr_alloc_info->ptr_first_free_segment % (10u * 10 * 10 * 10));
+
     }
 
     // create new segment_info object
     segment_info* ptr_current_segment = (segment_info*)ptr_alloc_info->ptr_first_free_segment;
     ptr_current_segment->ptr_page = (void*)ptr_alloc_info;
-    ptr_current_segment->size = size_data + size_segment_info;
+    ptr_current_segment->size = size_user_data + sizeof(segment_info);
 
     printf("\nsegment_info:\n");
-    printf("\t-> address: %lu\n", (long)ptr_current_segment);
-    printf("\t-> page ptr: %lu\n", (long)ptr_current_segment->ptr_page);
+    printf("\t-> address: %lu\n", (long)ptr_current_segment % (10u * 10 * 10 * 10));
+    printf("\t-> page ptr: %lu\n", (long)ptr_current_segment->ptr_page % (10u * 10 * 10 * 10));
     printf("\t-> size: %d\n", ptr_current_segment->size);
 
     // update alloc_info object
@@ -66,34 +79,40 @@ void* my_malloc(const unsigned size) {
     ptr_alloc_info->ptr_first_free_segment += ptr_current_segment->size;
 
     printf("\nalloc_info:\n");
-    printf("\t-> address: %lu\n", (long)ptr_alloc_info);
     printf("\t-> actual size: %d\n", ptr_alloc_info->size_actual);
-    printf("\t-> total size: %d\n", ptr_alloc_info->size_total);
     printf("\t-> number of segments: %d\n", ptr_alloc_info->num_of_segments);
-    printf("\t-> first free segment: %lu\n", (long)ptr_alloc_info->ptr_first_free_segment);
+    printf("\t-> first free segment: %lu\n", (long)ptr_alloc_info->ptr_first_free_segment % (10u * 10 * 10 * 10));
 
-    // return [ address of info ] + [ size of info ]
-    return (void*)ptr_alloc_info;
+    // return [ address of segment ] + [ size of segment ]
+    return (void*)ptr_current_segment + sizeof(segment_info);
 }
 
 void my_free(void* ptr_memory) {
     // config
-    const unsigned size_alloc_info = sizeof(alloc_info);
-    void* ptr_data = ptr_memory - size_alloc_info;
-    alloc_info* ptr_alloc_info = (alloc_info*)ptr_data;
-    const unsigned size_data = ptr_alloc_info->size_total;
+    void* ptr_data = ptr_memory - sizeof(segment_info);
+    segment_info* ptr_current_segment = (segment_info*)ptr_data;
+    alloc_info* ptr_alloc_info = (alloc_info*)ptr_current_segment->ptr_page;
 
-    /* printf("\n-------- MY FREE --------\n");
-    printf("Memory address: %p\n", ptr_memory);
-    printf("Subtracted pointer address: %p\n", ptr_alloc_info);
-    printf("alloc_info->size_actual: %d\n", ptr_alloc_info->size_actual);
-    printf("alloc_info->size_total: %d\n", ptr_alloc_info->size_total); */
+    // decrement number of segments, do not delete anything explicitly yet
+    ptr_alloc_info->num_of_segments--;
 
-    // deallocate memory
-    const int result = munmap(ptr_data, size_data);
-    if (result == -1)
-        perror("munmap");
+    printf("\n---------- MY FREE ---------");
+    printf("\nalloc_info:\n");
+    printf("\t-> address: %lu\n", (long)ptr_alloc_info % (10u * 10 * 10 * 10));
+    printf("\t-> number of segments: %d\n", ptr_alloc_info->num_of_segments);
+    printf("\nsegment_info:\n");
+    printf("\t-> address: %lu\n", (long)ptr_current_segment % (10u * 10 * 10 * 10));
 
-    // no dangling pointers
-    ptr_data = NULL;
+    // if no segments available, then deallocate memory
+    if (!ptr_alloc_info->num_of_segments) {
+        const int result = munmap((void*)ptr_alloc_info, ptr_alloc_info->size_total);
+        if (result == -1)
+            perror("munmap");
+
+        // no dangling pointers
+        ptr_alloc_info = NULL;
+
+        printf("Memory unmapped.\n");
+        return;
+    }
 }
